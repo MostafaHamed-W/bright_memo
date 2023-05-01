a645db58d00049cf9db28a89bd3930fe382f38fa
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-05-01 13:06:06 +0300

    add fields to notes bottom sheet

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kItemColor = Color(0xFFffcb77);
+const kPrimaryColor = Color(0xFF2fead7);
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:notes/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: const [
+          SizedBox(height: 35),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
