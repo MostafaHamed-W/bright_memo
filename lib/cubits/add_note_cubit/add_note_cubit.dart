84686e6b668a69b9a43fb59f7be6bed8d72853f3
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-05-16 12:15:40 +0300

    add note to cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes/constants.dart';
 
 import '../../models/note_model.dart';
 
@@ -8,5 +10,20 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(NoteModel note) {}
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSuccess());
+      await notesBox.add(note);
+    } on Exception catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
+
+// Generic - i can make a type t , in this case the add function can deal with any thing with any type|
+// if it takes int it can deal with it , or if it took any type it can deal with it
+  // add<T>(T name) {
+  //   //
+  // }
 }
